6feed652882c6d308492cfb2da5541e5c55104c6
江峰 <jiangfeng@morefungroup.com>
2021-11-12 15:13:55 +0800

    toString转换为json

--- jf-common-utils-starter/src/main/java/com/jf/common/utils/utils/string/ToStringDataToJson.java
+++ jf-common-utils-starter/src/main/java/com/jf/common/utils/utils/string/ToStringDataToJson.java
@@ -1,13 +1,21 @@
 package com.jf.common.utils.utils.string;
 
+import com.jf.common.utils.utils.file.FileUtils;
+
+import java.text.ParseException;
+
 /**
  * lombok  toString转化json
  */
 public class ToStringDataToJson {
 
-    public static void main(String[] args) {
-        // String str = "111";
-        String str = "id=1, name=江峰";
-        System.out.println(StringJsonUtils.toJsonStringByOneObject(str));
+    public static void main(String[] args) throws ParseException {
+
+        // String str = "(id=1, name=null)";
+
+        String str = FileUtils.readFileToString("E:\\code\\jf-common-repository\\jf-common-utils-starter\\src\\main\\java\\com\\jf\\common\\utils\\utils\\string\\string字符串");
+
+        System.out.println(StringToJsonUtils.toJSONString(str));
+
     }
 }
