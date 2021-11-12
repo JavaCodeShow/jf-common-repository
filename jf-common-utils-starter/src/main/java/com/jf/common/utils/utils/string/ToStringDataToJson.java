5125e03ea05ec75e9b6b53569b299de83ae0d85a
江峰 <jiangfeng@morefungroup.com>
2021-11-12 15:36:45 +0800

    toString转换为json

--- jf-common-utils-starter/src/main/java/com/jf/common/utils/utils/string/ToStringDataToJson.java
+++ jf-common-utils-starter/src/main/java/com/jf/common/utils/utils/string/ToStringDataToJson.java
@@ -15,6 +15,8 @@ public class ToStringDataToJson {
 
         String str = FileUtils.readFileToString("E:\\code\\jf-common-repository\\jf-common-utils-starter\\src\\main\\java\\com\\jf\\common\\utils\\utils\\string\\string字符串");
 
+        // 对象：直接复制进文本即可
+        // 数组：如：[User(id=1, name=张三), User(id=2, name=李四)]  需改为：(xxx=[User(id=1, name=张三), User(id=2, name=李四)])
         System.out.println(StringToJsonUtils.toJSONString(str));
 
     }
