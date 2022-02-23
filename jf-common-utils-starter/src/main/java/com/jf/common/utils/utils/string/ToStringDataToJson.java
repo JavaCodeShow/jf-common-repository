2cb1582bcc2400798b42720f7597377e244cf70a
江峰 <jiangfeng@morefungroup.com>
2022-02-23 13:11:59 +0800

    代码优化

--- jf-common-utils-starter/src/main/java/com/jf/common/utils/utils/string/ToStringDataToJson.java
+++ jf-common-utils-starter/src/main/java/com/jf/common/utils/utils/string/ToStringDataToJson.java
@@ -9,16 +9,16 @@ import java.text.ParseException;
  */
 public class ToStringDataToJson {
 
-	public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException {
 
 		// String str = "(id=1, name=null)";
 
 		String str = FileUtils.readFileToString(
-				"E:\\code\\jf-common-repository\\jf-common-utils-starter\\src\\main\\java\\com\\jf\\common\\utils\\utils\\string\\string字符串");
+				"E:\\code\\jf-common-repository\\jf-common-utils-starter\\src\\main\\java\\com\\jf\\common\\utils\\utils\\string\\string字符串.txt");
 
 		// 对象：直接复制进文本即可
-		// 数组：如：[User(id=1, name=张三), User(id=2, name=李四)] 需改为：(xxx=[User(id=1,
-		// name=张三), User(id=2, name=李四)])
+		// 数组：如：[User(id=1, name=张三), User(id=2, name=李四)]
+		// 需改为：(xxx=[User(id=1, name=张三), User(id=2, name=李四)])
 		System.out.println(StringToJsonUtils.toJSONString(str));
 
 	}
