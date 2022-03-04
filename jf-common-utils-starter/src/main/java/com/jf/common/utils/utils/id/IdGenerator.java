b3b8b23af8c7fbea77e559ba9b846bb44c1c0171
江峰 <jiangfeng@morefungroup.com>
2022-03-04 17:37:11 +0800

    ID生成器

---
+++ jf-common-utils-starter/src/main/java/com/jf/common/utils/utils/id/IdGenerator.java
@@ -0,0 +1,18 @@
+package com.jf.common.utils.utils.id;
+
+/**
+ * ID发号器：生成唯一的并且趋势自增的ID
+ *
+ * @author 江峰
+ * @date 2022/3/4 17:13
+ */
+public final class IdGenerator {
+
+    /**
+     * 获取一个唯一的并且趋势自增的ID
+     */
+    public static String getId() {
+        return new ObjectId().toString();
+    }
+
+}
